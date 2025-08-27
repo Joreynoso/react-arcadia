1025b0af50304e50610b164aa987924522f21a26
Joreynoso <joreynoso.developer@gmail.com>
2025-08-26 22:32:19 -0300

    Edit connectDB mongo

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -9,7 +9,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(mongo_uri)
     console.log("--> [DATABASE] Conectado a MongoDB")
-    console.log("--> [DATABASE] contectado a:" , mongo_uri)
   } catch (error) {
     console.error("Error al conectar a MongoDB:", error.message)
     process.exit(1)
