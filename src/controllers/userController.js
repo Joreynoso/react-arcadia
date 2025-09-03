8cb0278959d947cd31d3ede9c8d50b181cfe56c5
Joreynoso <joreynoso.developer@gmail.com>
2025-09-03 17:19:53 -0300

    implement /me rout to get roles & permissions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,58 +1,89 @@
-import UserService from '../services/userService.js'
+import UserService from "../services/userService.js"
+import User from "../models/User.js"
 
 class UserController {
-    // --> obtener perfil del usuario
-    async getOwnProfile(req, res) {
-        try {
-            const user = await UserService.getUser(req.user.id)
+  // --> obtener perfil del usuario
+  async getOwnProfile(req, res) {
+    try {
+      const user = await UserService.getUser(req.user.id)
 
-            console.log('1. usario proveniente del middleware', user)
+      console.log("1. usario proveniente del middleware", user)
 
-            res.status(200).json({ succes: true, message: 'Perfil del usuario', user })
-        } catch (error) {
-            console.error(error)
-            res.status(500).json({ succes: false, message: error.message })
-        }
+      res.status(200).json({ succes: true, message: "Perfil del usuario", user })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ succes: false, message: error.message })
     }
-    // --> lista de usuarios
-    async getAll(req, res) {
-        try {
-            const users = await UserService.listUsers()
-            if (!users || users.length === 0) {
-                return res.status(404).json({ succes: false, message: 'no se encontraro usuarios' })
-            }
-            res.status(200).json({ succes: true, message: 'Lista de usuarios', users })
-        } catch (error) {
-            console.error(error)
-            res.status(500).json({ succes: false, message: error.message })
-        }
+  }
+  // --> lista de usuarios
+  async getAll(req, res) {
+    try {
+      const users = await UserService.listUsers()
+      if (!users || users.length === 0) {
+        return res.status(404).json({ succes: false, message: "no se encontraro usuarios" })
+      }
+      res.status(200).json({ succes: true, message: "Lista de usuarios", users })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ succes: false, message: error.message })
     }
+  }
 
-    // --> buscar un urusario por id
-    async getById(req, res) {
-        try {
-            const { id } = req.params
-            const user = await UserService.getUser(id)
-            if (!user) return res.status(404).json({ succes: false, message: 'Usuario no encontrado' })
-            res.status(200).json({ succes: true, message: 'Usuario encontrado', user })
-        } catch (error) {
-            console.error(error)
-            res.status(500).json({ succes: false, message: error.message })
-        }
+  // --> buscar un urusario por id
+  async getById(req, res) {
+    try {
+      const { id } = req.params
+      const user = await UserService.getUser(id)
+      if (!user) return res.status(404).json({ succes: false, message: "Usuario no encontrado" })
+      res.status(200).json({ succes: true, message: "Usuario encontrado", user })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ succes: false, message: error.message })
     }
+  }
 
-    // --> eliminar un usuario
-    async delete(req, res) {
-        try {
-            const { id } = req.params
-            const user = await UserService.deletUser(id)
-            if (!user) return res.status(404).json({ succes: false, message: 'Usuario no encontrado' })
-            res.status(200).json({ succes: true, message: 'Usuario eliminado', user })
-        } catch (error) {
-            console.error(error)
-            res.status(500).json({ succes: false, message: error.message })
-        }
+  // --> eliminar un usuario
+  async delete(req, res) {
+    try {
+      const { id } = req.params
+      const user = await UserService.deletUser(id)
+      if (!user) return res.status(404).json({ succes: false, message: "Usuario no encontrado" })
+      res.status(200).json({ succes: true, message: "Usuario eliminado", user })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ succes: false, message: error.message })
     }
+  }
+
+  // --> obtener usuario, rol, permisos.
+  async getMe(req, res) {
+    try {
+      // req.user.id viene del middleware JWT
+      const user = await User.findById(req.user.id).populate({
+        path: "role",
+        select: "name permissions",
+        populate: {
+          path: "permissions",
+          select: "name description",
+        },
+      })
+
+      if (!user) return res.status(404).json({ message: "Usuario no encontrado" })
+
+      // Extraemos solo los nombres de los permisos
+      const permissions = user.role.permissions.map((p) => p.name)
+
+      // Devolvemos al frontend solo la info necesaria
+      res.json({
+        id: user._id,
+        email: user.email,
+        role: user.role.name,
+        permissions,
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
 export default new UserController()
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from "express"
 import UserController from "../controllers/userController.js"
 import { authenticateToken, hasPermission } from "../middlewares/authMiddleware.js"
+import userController from "../controllers/userController.js"
 
 // instanciar Router
 const userRouter = express.Router()
 
 // rutas de los usuarios
 userRouter.get("/", authenticateToken, hasPermission("read:users"), UserController.getAll)
+userRouter.get("/me", authenticateToken, userController.getMe)
 userRouter.get("/profile", authenticateToken, hasPermission("read:profile"), UserController.getOwnProfile)
 userRouter.get("/:id", authenticateToken, hasPermission("read:users"), UserController.getById)
 userRouter.delete("/:id", authenticateToken, hasPermission("delete:user"), UserController.delete)
